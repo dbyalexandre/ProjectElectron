f4c786943f71b13bf8ebf6af47f97f3d44dbdd93
adaubigney <alexandre.daubigney@ynov.com>
2023-02-20 09:34:25 +0100

    first commit

---
+++ renderer.js
@@ -0,0 +1,16 @@
+import { dialog } from 'electron'
+
+const file = document.getElementById('chooseFile')
+
+function chooseFile() {
+  alert("test")
+}
+  // document.file.addEventListener('click', function() {
+  //   dialog.showOpenDialog({
+  //     properties: ['openFile']
+  //   }, function(files) {
+  //     if (files) {
+  //       console.log(`Fichier sélectionné : ${files[0]}`)
+  //     }
+  //   })
+  // })
